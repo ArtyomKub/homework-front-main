70bf1abd04948c9773dcde34a758623d3554e8be
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-23 14:24:18 +0200

    Complete Homewrok

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,28 +1,28 @@
-type AddressType = {
-    street: string; // ПОДПРАВЛЯЕМ any          DONE
-    city: string; // ПОДПРАВЛЯЕМ any            DONE
+export type AddressType = {
+    street: string; // ПОДПРАВЛЯЕМ any
+    city: string; // ПОДПРАВЛЯЕМ any
 };
 
-type UserType = {
+export type UserType = {
     id: number
     name: string
-    address: Array<AddressType>  //             DONE
+    age: number
+    address: AddressType
 };
 
-type UserListPropsType = {
-    users: Array<UserType>; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ... DONE
+export type UserListPropsType = {
+    users: Array<UserType>; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
 };
 
 export const UserList = (props: UserListPropsType) => {
     return (
         <div id={'hw01-users'}>
             <h2>User List:</h2>
-
             <ul>
                 {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-                    <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>
-                        <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>
-                        {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}
+                    <li key={user.id} id={`hw01-user-${user.id}`}>
+                        <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                        {user.address.street}, {user.address.city}
                     </li>
                 ))}
             </ul>
