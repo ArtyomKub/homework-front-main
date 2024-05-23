7b2544e1fbd05ade62cee96b95178f60b2126ddf
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-23 13:15:14 +0200

    Add typing in AdaressType, UserType

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,29 +1,31 @@
 type AddressType = {
-  street: any; // ПОДПРАВЛЯЕМ any
-  city: any; // ПОДПРАВЛЯЕМ any
+    street: string; // ПОДПРАВЛЯЕМ any          DONE
+    city: string; // ПОДПРАВЛЯЕМ any            DONE
 };
 
 type UserType = {
-  // ПРИДЕТСЯ САМОМУ)
+    id: number
+    name: string
+    address: Array<AddressType>  //             DONE
 };
 
 type UserListPropsType = {
-  users: Array<UserType>; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ... DONE
+    users: Array<UserType>; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ... DONE
 };
 
 export const UserList = (props: UserListPropsType) => {
-  return (
-    <div id={'hw01-users'}>
-      <h2>User List:</h2>
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
 
-      <ul>
-        {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-          <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>
-            <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>
-            {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+            <ul>
+                {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+                    <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>
+                        <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>
+                        {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
